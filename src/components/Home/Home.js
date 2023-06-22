fb3fcec9904122585639a9e1d0938da5fdc97a2c
CJ <cjcarnicle@gmail.com>
2023-06-22 16:25:37 -0500

    update for mobile header

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,13 +6,22 @@ import Home2 from "./Home2";
 import Type from "./Type";
 
 function Home() {
+
+  let homeClassName = 'home-header'; // Default class name
+
+  if (window.matchMedia('(max-width: 767px)').matches) {
+    homeClassName = 'home-header-mobile'; // Mobile-specific class name
+  }
+
+  console.log("home class name: ", homeClassName)
+
   return (
     <section>
       <Container fluid className="home-section" id="home">
         <Particle />
         <Container className="home-content">
           <Row>
-            <Col md={7} className="home-header">
+            <Col md={7} className={homeClassName}>
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Hi There!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
